006d975fb3518da4a3fea848eb707ef859fa92d9
demo <praveen98845kumar@gmail.com>
2026-07-19 12:59:18 +0530

    Initial Express backend setup

---
+++ devflow-backend/src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to DevFlow API"
+    });
+});
+
+module.exports = app;
